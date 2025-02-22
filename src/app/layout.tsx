9686d1a707ae4f1d4d4b0d4f2675b51a8ee46a9c
Steve Brown <brown2020@gmail.com>
2025-02-22 14:49:16 -0800

    fix style loc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 // src/app/layout.tsx
 import type { Metadata } from "next";
-import "../styles/globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "OpenTetris Game",
